5a91ee23f29102c3006d5a5d6670604b0d70450c
karolhof <2303211@ucc.edu.ar>
2025-05-18 22:56:14 -0300

    implementación de mock

---
+++ frontend/src/pages/ActivityPage.jsx
@@ -0,0 +1,16 @@
+// src/pages/ActivityPage.jsx
+import React from 'react';
+import ActivityDetail from '../components/ActivityDetail';
+import '../styles/activityPack.css';
+import IconPack from '../components/IconPack';
+
+const ActivityPage = () => {
+  return (
+    <div className="activity-page">
+      <ActivityDetail />
+      <IconPack/>
+    </div>
+  );
+};
+
+export default ActivityPage;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ActivityCard from '../components/ActivityCard';
+import '../styles/home.css';
+import IconPack from '../components/IconPack';
+import SearchPack from '../components/SearchPack';
+import GymHeader from '../components/GymHeader';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+        <IconPack/>
+        <SearchPack/>
+        <GymHeader/>
+    </div>
+  );
+};
+
+export default Home;
